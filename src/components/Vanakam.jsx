b3ce225482b4855997237e678026ec6e807fcdd1
Adithyaa Saravanam <adithyaasaravanam@gmail.com>
2025-06-20 13:34:19 +0530

    Added Greeting

---
+++ src/components/Vanakam.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Vanakam() {
+  return (
+    <>
+      <h1>Vanakam da Mapla !</h1>
+      <h1>Gok Gok Gokul !!!</h1>
+    </>
+  );
+}
+
+export default Vanakam;
