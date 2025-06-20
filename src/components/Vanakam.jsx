4d743cc74caceb7c12037f8dd62205461ef3e890
GOKULRAJ-L <gokulrajlakshmananit@gmail.com>
2025-06-20 13:53:30 +0530

    Greetings Modified

--- src/components/Vanakam.jsx
+++ src/components/Vanakam.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Vanakam() {
   return (
     <>
-      <h1>Vanakam da Sunni !</h1>
+      <h1>Vanakam   da Sunni !</h1>
       <h1> AAAA Adithyaa!!!</h1>
     </>
   );
